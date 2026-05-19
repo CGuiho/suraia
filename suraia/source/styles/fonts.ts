469d77fb811974fe8afd1d7a69526cc7792f7cb9
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-20 01:11:59 +0200

    feat(styles): add companion fonts.ts

---
+++ suraia/source/styles/fonts.ts
@@ -0,0 +1,26 @@
+/**
+ * @copyright Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+const googleSansFlex = "'Google Sans Flex', sans-serif" as const;
+const firaSans = "'Fira Sans', sans-serif" as const;
+const firaSansCondensed = "'Fira Sans Condensed', sans-serif" as const;
+const firaSansExtraCondensed = "'Fira Sans Extra Condensed', sans-serif" as const;
+const jetbrainsMono = "'JetBrains Mono', monospace" as const;
+const cookie = "'Cookie', cursive" as const;
+
+export const FONT = {
+  googleSansFlex,
+  firaSans,
+  firaSansCondensed,
+  firaSansExtraCondensed,
+  jetbrainsMono,
+  cookie,
+
+  fontLogo: googleSansFlex,
+  fontSans: firaSans,
+  fontSansCondensed: firaSansCondensed,
+  fontSansExtraCondensed: firaSansExtraCondensed,
+  fontMono: jetbrainsMono,
+  fontCursive: cookie,
+} as const;
