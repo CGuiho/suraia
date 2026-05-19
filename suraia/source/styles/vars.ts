daadf2e3b75cb2d5d7e5cd4f6b36935aa29f7e24
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-20 01:12:07 +0200

    feat(styles): add companion vars.ts

---
+++ suraia/source/styles/vars.ts
@@ -0,0 +1,9 @@
+/**
+ * @copyright Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+const PANEL_WIDTH_COLLAPSED = 72 as const;
+const PANEL_WIDTH_OPEN = 240 as const;
+
+export { PANEL_WIDTH_COLLAPSED, PANEL_WIDTH_OPEN };
+export type { };
