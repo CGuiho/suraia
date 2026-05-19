e87a279f3a53d11bc6b44e1e0ccac42ac942b528
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-20 01:09:31 +0200

    feat(draft): add C:/GUIHO/suraira/draft/styles/colors/colors.ts

---
+++ draft/styles/colors/colors.ts
@@ -0,0 +1,271 @@
+/*!
+ * Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO
+ * All Rights Reserved.
+ */
+
+export const white = '#ffffff' as const
+export const black = '#000000' as const
+
+export const glassLight_0 = 'rgba(255, 255, 255, 0.05)' as const
+export const glassLight_1 = 'rgba(255, 255, 255, 0.10)' as const
+export const glassLight_2 = 'rgba(255, 255, 255, 0.15)' as const
+export const glassLight_3 = 'rgba(255, 255, 255, 0.20)' as const
+export const glassLight_4 = 'rgba(255, 255, 255, 0.25)' as const
+export const glassLight_5 = 'rgba(255, 255, 255, 0.30)' as const
+export const glassLight_6 = 'rgba(255, 255, 255, 0.35)' as const
+export const glassLight_7 = 'rgba(255, 255, 255, 0.40)' as const
+export const glassLight_8 = 'rgba(255, 255, 255, 0.45)' as const
+export const glassLight_9 = 'rgba(255, 255, 255, 0.50)' as const
+
+export const glassDark_0 = 'rgba(0, 0, 0, 0.05)' as const
+export const glassDark_1 = 'rgba(0, 0, 0, 0.10)' as const
+export const glassDark_2 = 'rgba(0, 0, 0, 0.15)' as const
+export const glassDark_3 = 'rgba(0, 0, 0, 0.20)' as const
+export const glassDark_4 = 'rgba(0, 0, 0, 0.25)' as const
+export const glassDark_5 = 'rgba(0, 0, 0, 0.30)' as const
+export const glassDark_6 = 'rgba(0, 0, 0, 0.35)' as const
+export const glassDark_7 = 'rgba(0, 0, 0, 0.40)' as const
+export const glassDark_8 = 'rgba(0, 0, 0, 0.45)' as const
+export const glassDark_9 = 'rgba(0, 0, 0, 0.50)' as const
+
+export const guihoLight_0 = '#fafaff' as const
+export const guihoLight_1 = '#f6f6f8' as const
+export const guihoLight_2 = '#f0f0f2' as const
+export const guihoLight_3 = '#eeeeef' as const
+export const guihoLight_4 = '#e8e8ea' as const
+export const guihoLight_5 = '#dededf' as const
+export const guihoLight_6 = '#d4d4d6' as const
+export const guihoLight_7 = '#cececf' as const
+export const guihoLight_8 = '#ffffff' as const // Main Color
+export const guihoLight_9 = '#bebebf' as const
+
+export const guihoDark_0 = '#A6A7AB' as const
+export const guihoDark_1 = '#909296' as const
+export const guihoDark_2 = '#5C5F66' as const
+export const guihoDark_3 = '#373A40' as const
+export const guihoDark_4 = '#2C2E33' as const
+export const guihoDark_5 = '#25262B' as const
+export const guihoDark_6 = '#000000' as const // Main Color -- #C1C2C5
+export const guihoDark_7 = '#1A1B1E' as const
+export const guihoDark_8 = '#141517' as const
+export const guihoDark_9 = '#101113' as const
+
+export const light_0 = '#ffffff' as const
+export const light_1 = '#fafaff' as const
+export const light_2 = '#f6f6f8' as const
+export const light_3 = '#f0f0f2' as const
+export const light_4 = '#eeeeef' as const
+export const light_5 = '#e8e8ea' as const
+export const light_6 = '#dededf' as const
+export const light_7 = '#c4c4c6' as const
+export const light_8 = '#bebebf' as const
+export const light_9 = '#aeaeaf' as const
+
+export const dark_0 = '#6a6a6b' as const
+export const dark_1 = '#505256' as const
+export const dark_2 = '#4c4f56' as const
+export const dark_3 = '#373a40' as const
+export const dark_4 = '#2c2e33' as const
+export const dark_5 = '#25262b' as const
+export const dark_6 = '#1a1b1e' as const
+export const dark_7 = '#141517' as const
+export const dark_8 = '#101113' as const
+export const dark_9 = '#000000' as const
+
+export const brown_0 = '#f7f3f2' as const
+export const brown_1 = '#e8e6e5' as const
+export const brown_2 = '#d2c9c6' as const
+export const brown_3 = '#bdaaa4' as const
+export const brown_4 = '#ab9087' as const
+export const brown_5 = '#a17f74' as const
+export const brown_6 = '#9d766a' as const
+export const brown_7 = '#896459' as const
+export const brown_8 = '#7b594e' as const
+export const brown_9 = '#5d4037' as const
+
+export const same_0 = '#dddddd' as const
+export const same_1 = '#cccccc' as const
+export const same_2 = '#bbbbbb' as const
+export const same_3 = '#aaaaaa' as const
+export const same_4 = '#999999' as const
+export const same_5 = '#888888' as const
+export const same_6 = '#777777' as const
+export const same_7 = '#666666' as const
+export const same_8 = '#555555' as const
+export const same_9 = '#444444' as const
+
+export const gray_0 = '#f8f9fa' as const
+export const gray_1 = '#f1f3f5' as const
+export const gray_2 = '#e9ecef' as const
+export const gray_3 = '#dee2e6' as const
+export const gray_4 = '#ced4da' as const
+export const gray_5 = '#adb5bd' as const
+export const gray_6 = '#868e96' as const
+export const gray_7 = '#596067' as const
+export const gray_8 = '#444a50' as const
+export const gray_9 = '#313539' as const
+
+export const blueGray_0 = '#f3f3fe' as const
+export const blueGray_1 = '#e4e6ed' as const
+export const blueGray_2 = '#c8cad3' as const
+export const blueGray_3 = '#a9adb9' as const
+export const blueGray_4 = '#9093a4' as const
+export const blueGray_5 = '#808496' as const
+export const blueGray_6 = '#767c91' as const
+export const blueGray_7 = '#656a7e' as const
+export const blueGray_8 = '#585e72' as const
+export const blueGray_9 = '#4a5167' as const
+
+export const red_0 = '#fff5f5' as const
+export const red_1 = '#ffe3e3' as const
+export const red_2 = '#ffc9c9' as const
+export const red_3 = '#ffa8a8' as const
+export const red_4 = '#ff8787' as const
+export const red_5 = '#ff6b6b' as const
+export const red_6 = '#fa5252' as const
+export const red_7 = '#f03e3e' as const
+export const red_8 = '#e03131' as const
+export const red_9 = '#c92a2a' as const
+
+export const pink_0 = '#fff0f6' as const
+export const pink_1 = '#ffdeeb' as const
+export const pink_2 = '#fcc2d7' as const
+export const pink_3 = '#faa2c1' as const
+export const pink_4 = '#f783ac' as const
+export const pink_5 = '#f06595' as const
+export const pink_6 = '#e64980' as const
+export const pink_7 = '#d6336c' as const
+export const pink_8 = '#c2255c' as const
+export const pink_9 = '#a61e4d' as const
+
+export const brightPink_0 = '#F0BBDD' as const
+export const brightPink_1 = '#ED9BCF' as const
+export const brightPink_2 = '#EC7CC3' as const
+export const brightPink_3 = '#ED5DB8' as const
+export const brightPink_4 = '#F13EAF' as const
+export const brightPink_5 = '#F71FA7' as const
+export const brightPink_6 = '#FF00A1' as const
+export const brightPink_7 = '#E00890' as const
+export const brightPink_8 = '#C50E82' as const
+export const brightPink_9 = '#AD1374' as const
+
+export const grape_0 = '#f8f0fc' as const
+export const grape_1 = '#f3d9fa' as const
+export const grape_2 = '#eebefa' as const
+export const grape_3 = '#e599f7' as const
+export const grape_4 = '#da77f2' as const
+export const grape_5 = '#cc5de8' as const
+export const grape_6 = '#be4bdb' as const
+export const grape_7 = '#ae3ec9' as const
+export const grape_8 = '#9c36b5' as const
+export const grape_9 = '#862e9c' as const
+
+export const violet_0 = '#f3f0ff' as const
+export const violet_1 = '#e5dbff' as const
+export const violet_2 = '#d0bfff' as const
+export const violet_3 = '#b197fc' as const
+export const violet_4 = '#9775fa' as const
+export const violet_5 = '#845ef7' as const
+export const violet_6 = '#7950f2' as const
+export const violet_7 = '#7048e8' as const
+export const violet_8 = '#6741d9' as const
+export const violet_9 = '#5f3dc4' as const
+
+export const indigo_0 = '#edf2ff' as const
+export const indigo_1 = '#dbe4ff' as const
+export const indigo_2 = '#bac8ff' as const
+export const indigo_3 = '#91a7ff' as const
+export const indigo_4 = '#748ffc' as const
+export const indigo_5 = '#5c7cfa' as const
+export const indigo_6 = '#4c6ef5' as const
+export const indigo_7 = '#4263eb' as const
+export const indigo_8 = '#3b5bdb' as const
+export const indigo_9 = '#364fc7' as const
+
+export const blue_0 = '#e7f5ff' as const
+export const blue_1 = '#d0ebff' as const
+export const blue_2 = '#a5d8ff' as const
+export const blue_3 = '#74c0fc' as const
+export const blue_4 = '#4dabf7' as const
+export const blue_5 = '#339af0' as const
+export const blue_6 = '#228be6' as const
+export const blue_7 = '#1c7ed6' as const
+export const blue_8 = '#1971c2' as const
+export const blue_9 = '#1864ab' as const
+
+export const blueBright_0 = '#e5f3ff' as const
+export const blueBright_1 = '#cde2ff' as const
+export const blueBright_2 = '#9ac2ff' as const
+export const blueBright_3 = '#64a0ff' as const
+export const blueBright_4 = '#3884fe' as const
+export const blueBright_5 = '#1d72fe' as const
+export const blueBright_6 = '#0969ff' as const
+export const blueBright_7 = '#0058e4' as const
+export const blueBright_8 = '#004ecd' as const
+export const blueBright_9 = '#0043b5' as const
+
+export const cyan_0 = '#e3fafc' as const
+export const cyan_1 = '#c5f6fa' as const
+export const cyan_2 = '#99e9f2' as const
+export const cyan_3 = '#66d9e8' as const
+export const cyan_4 = '#3bc9db' as const
+export const cyan_5 = '#22b8cf' as const
+export const cyan_6 = '#15aabf' as const
+export const cyan_7 = '#1098ad' as const
+export const cyan_8 = '#0c8599' as const
+export const cyan_9 = '#0b7285' as const
+
+export const teal_0 = '#e6fcf5' as const
+export const teal_1 = '#c3fae8' as const
+export const teal_2 = '#96f2d7' as const
+export const teal_3 = '#63e6be' as const
+export const teal_4 = '#38d9a9' as const
+export const teal_5 = '#20c997' as const
+export const teal_6 = '#12b886' as const
+export const teal_7 = '#0ca678' as const
+export const teal_8 = '#099268' as const
+export const teal_9 = '#087f5b' as const
+
+export const green_0 = '#ebfbee' as const
+export const green_1 = '#d3f9d8' as const
+export const green_2 = '#b2f2bb' as const
+export const green_3 = '#8ce99a' as const
+export const green_4 = '#69db7c' as const
+export const green_5 = '#51cf66' as const
+export const green_6 = '#40c057' as const
+export const green_7 = '#37b24d' as const
+export const green_8 = '#2f9e44' as const
+export const green_9 = '#2b8a3e' as const
+
+export const lime_0 = '#f4fce3' as const
+export const lime_1 = '#e9fac8' as const
+export const lime_2 = '#d8f5a2' as const
+export const lime_3 = '#c0eb75' as const
+export const lime_4 = '#a9e34b' as const
+export const lime_5 = '#94d82d' as const
+export const lime_6 = '#82c91e' as const
+export const lime_7 = '#74b816' as const
+export const lime_8 = '#66a80f' as const
+export const lime_9 = '#5c940d' as const
+
+export const yellow_0 = '#fff9db' as const
+export const yellow_1 = '#fff3bf' as const
+export const yellow_2 = '#ffec99' as const
+export const yellow_3 = '#ffe066' as const
+export const yellow_4 = '#ffd43b' as const
+export const yellow_5 = '#fcc419' as const
+export const yellow_6 = '#fab005' as const
+export const yellow_7 = '#f59f00' as const
+export const yellow_8 = '#f08c00' as const
+export const yellow_9 = '#e67700' as const
+
+export const orange_0 = '#fff4e6' as const
+export const orange_1 = '#ffe8cc' as const
+export const orange_2 = '#ffd8a8' as const
+export const orange_3 = '#ffc078' as const
+export const orange_4 = '#ffa94d' as const
+export const orange_5 = '#ff922b' as const
+export const orange_6 = '#fd7e14' as const
+export const orange_7 = '#f76707' as const
+export const orange_8 = '#e8590c' as const
+export const orange_9 = '#d9480f' as const
