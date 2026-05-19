e24ab9e42340254c0c886198c79db92638a89895
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-20 01:26:26 +0200

    style(delete): remove vars.ts as it is useless

--- suraia/source/styles/vars.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @copyright Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
- */
-
-// App-specific panel dimensions have been removed. Use the central design tokens instead.
-export type { };
