67e33ce2f03572765b18acc95d22946a6032fbbd
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-25 23:38:45 +0200

    feat(blueprint): add kbd.ts

---
+++ suraia/source/components/kbd/kbd.ts
@@ -0,0 +1,12 @@
+/**
+ * @copyright Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+export { KbdController };
+export type { KbdControllerOptions };
+
+interface KbdControllerOptions {}
+
+class KbdController {
+  constructor(_options: KbdControllerOptions = {}) {}
+}
