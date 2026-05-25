dbbdd85318f437cc9dd14d77c39b6539cb2146c7
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-26 01:39:55 +0200

    feat(blueprint): add rolling-number.ts

--- suraia/source/components/rolling-number/rolling-number.ts
+++ suraia/source/components/rolling-number/rolling-number.ts
@@ -65,7 +65,10 @@ class RollingNumberController {
 
   public getDigitOffset(digit: string): number {
     const parsed = parseInt(digit, 10);
-    return isNaN(parsed) ? 0 : parsed * -10; // offset in percentage, e.g. -20% for digit 2
+    if (Number.isNaN(parsed) || parsed === 0) {
+      return 0;
+    }
+    return parsed * -10; // offset in percentage, e.g. -20% for digit 2
   }
 
   public getDataAttributes(): Record<string, string | undefined> {
