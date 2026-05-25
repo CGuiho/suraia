31b3c63e1799f0686d5a9f015cebfcf08bca126d
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-25 23:38:57 +0200

    feat(blueprint): add visually-hidden.ts

---
+++ suraia/source/components/visually-hidden/visually-hidden.ts
@@ -0,0 +1,12 @@
+/**
+ * @copyright Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+export { VisuallyHiddenController };
+export type { VisuallyHiddenControllerOptions };
+
+interface VisuallyHiddenControllerOptions {}
+
+class VisuallyHiddenController {
+  constructor(_options: VisuallyHiddenControllerOptions = {}) {}
+}
