cbe0cb63539163b427222390ae0461d4839ea679
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-29 00:51:01 +0200

    preview: add workbench entrypoint

---
+++ suraia/preview/source/main.ts
@@ -0,0 +1,7 @@
+/** @copyright Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved. */
+
+import { initApp } from "./app";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
+});
